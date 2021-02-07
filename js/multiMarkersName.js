079b123357d7aff4e7ca1edc90b25273887390b2
warlocklama <64942522+warlocklama@users.noreply.github.com>
2021-02-07 14:06:27 +0545

    Update multiMarkersName.js

--- js/multiMarkersName.js
+++ js/multiMarkersName.js
@@ -1,5 +1,38 @@
-AFRAME.registerComponent('markers_start', {
-  init: function () {
-    console.log('Hello, World!');
+var markersURLArray=[];
+var markersNameArray=[];
+
+AFRAME.registerComponent('markers_start',{
+  init:function(){
+    console.log('Add markers to the scene');
+    var sceneEl = document.querySelector('a-scene');
+    
+    //lists of the markers
+    for(var i=1; i<19; i++)
+    {
+      var url="resources/markers/pattern-Individual_Blocks-"+i+".patt";
+      markersURLArray.push(url);
+      markersNameArray.push('Marker_'+i);      
+    }
+
+    for(var k=0; k<18; k++)
+    {
+      var markerEl = document.createElement('a-marker');
+      markerEl.setAttribute('type','pattern');
+      markerEl.setAttribute('url',markersURLArray[k]);
+      markerEl.setAttribute('id',markersNameArray[k]);
+
+      markerEl.setAttribute('registerevents','');
+      sceneEl.appendChild(markerEl);
+
+      //Adding a text to each marker
+      var textEl = document.createElement('a-entity');
+      
+      textEl.setAttribute('id','text');
+      textEl.setAttribute('text',{color: 'red', align: 'center', value:markersNameArray[k], width: '5.5'});
+      textEl.object3D.position.set(0, 0.7, 0);
+      textEl.object3D.rotation.set(-90, 0, 0);
+
+      markerEl.appendChild(textEl);
+    }
   }
 });
